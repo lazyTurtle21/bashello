7614c7ad297f276e4ed57cfc1a72f6e96146eea9
neverlandjt <tymchenko@ucu.edu.ua>
2019-11-11 02:25:31 +0200

    update main;

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include <boost/filesystem.hpp>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include "../inc/wildcards.hpp"
+#include "../inc/command.hpp"
 #include "../inc/builtins.hpp"
 #include "../inc/custom_environ.hpp"
 #include "../inc/buf_containers.hpp"
@@ -20,11 +20,27 @@
 #define PATH_COLOR "\u001b[38;5;198m"
 #define RESET "\u001b[0m"
 
+void execute(command &command_, arguments_container &arguments, custom_environ &environ_, int &status) {
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork failed");
+        exit(1);
+    } else if (pid == 0) {
+        int ret = execvpe(command_.args[0].c_str(), (char **) arguments.buf, environ_.c_arr());
+        if (ret == -1 && errno == ENOENT) {     // no such file or directory
+            std::cerr << ERR_COLOR << command_.args[0] << ": command not found" << RESET << '\n';
+        }
+        _exit(0);
+    } else {
+        // IFKA na waitpid
+        (void) waitpid(pid, &status, 0);
+    }
+}
+
 
 int main(int argc, char *argv[], char *envp[]) {
     std::string string_args, full_path, dir, filename;
     std::string current_path = boost::filesystem::current_path().string();
-    std::vector<std::string> command;
     command_line cmd_line;
     int status = 0;
 
@@ -36,53 +52,48 @@ int main(int argc, char *argv[], char *envp[]) {
 
     arguments_container arguments;
 
-
     while ((cmd_line.buf = readline((PATH_COLOR + current_path + RESET + DOLLAR + "$ " + RESET).c_str())) != nullptr) {
         if (strlen(cmd_line.buf) > 0) add_history(cmd_line.buf);
 
         string_args = cmd_line.buf;
-        command.clear();
 
-        split(string_args, command);
-        replace_vars(command);
-        status = parse_variables(command, environ_);
-        if (status){
+        // TODO: modify split dlya pipes i redirections i scriptiv
+        auto command_ = split(string_args);
+
+// Вася так сложно, я завтикала на моменті що ми спочатку заміняємо варс а потім їх парсимо
+//Поки хай буде то шо реплейс работає з доларом а парс з дорівнює
+
+// TODO: rename this chudo fot better code understanding
+        replace_vars(command_.args);
+        status = parse_variables(command_.args, environ_);
+        if (status) {
             std::cerr << ERR_COLOR << "error while creating a new variable: " << strerror(status) << RESET << '\n';
             continue;
         }
-        if (command.empty()) continue;
+        if (command_.args.empty()) continue;
 
-        replace_wildcard(command, full_path, dir, filename);
+        replace_wildcard(command_.args, full_path, dir, filename);
 
-        if (builtins.find(command[0]) != builtins.end()) {
-            status = builtins[command[0]](command);
-            if (status){
-                std::cerr << ERR_COLOR << command[0] <<": " << strerror(status) << RESET << '\n';
+
+
+        // TODO: change to ptosto command vseredyni
+        if (builtins.find(command_.args[0]) != builtins.end()) {
+            status = builtins[command_.args[0]](command_.args);
+            if (status) {
+                std::cerr << ERR_COLOR << command_.args[0] << ": " << strerror(status) << RESET << '\n';
             }
-
             continue;
         }
-        arguments.buf = new const char *[command.size() + 1];   // extra room for sentinel
 
-        for (int j = 0; j < command.size(); ++j) {     // copy args
-            arguments.buf[j] = command[j].c_str();
+
+        arguments.buf = new const char *[command_.args.size() + 1];   // extra room for sentinel
+        for (int j = 0; j < command_.args.size(); ++j) {     // copy args
+            arguments.buf[j] = command_.args[j].c_str();
         }
+        arguments.buf[command_.args.size()] = nullptr;  // end of arguments sentinel is NULL
 
-        arguments.buf[command.size()] = nullptr;  // end of arguments sentinel is NULL
+        execute(command_, arguments, environ_, status);
 
-        pid_t pid = fork();
-        if (pid == -1) {
-            perror("fork failed");
-            exit(1);
-        } else if (pid == 0) {
-            int ret = execvpe(command[0].c_str(), (char **) arguments.buf, environ_.c_arr());
-            if (ret == -1 && errno == ENOENT) {     // no such file or directory
-                std::cerr << ERR_COLOR << command[0] << ": command not found" << RESET << '\n';
-            }
-            _exit(0);
-        } else {
-            (void) waitpid(pid, &status, 0);
-        }
     }
 
     return 0;
