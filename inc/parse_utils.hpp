25a9f352973c00b81d93f84307201e536e49c50b
sophia <sophiezhyrovetska@gmail.com>
2019-11-18 21:13:21 +0200

    Add new parse_utils

--- inc/parse_utils.hpp
+++ inc/parse_utils.hpp
@@ -11,11 +11,13 @@
 #include "custom_environ.hpp"
 #include "command.hpp"
 
-void replace_vars(std::vector<std::string>& command);
-
+void replace_vars(std::string& command);
+std::vector<std::string> get_script_vars(std::string& command);
 void split(std::string &source, std::vector<std::string> &destination);
 
 void replace_wildcard(std::vector<std::string>& command, std::string& full_path,  std::string& dir);
 
 int parse_variables(std::vector<std::string>& command, custom_environ &environ_);
+std::string remove_leading_ending_spaces(const std::string& str);
+std::pair<std::string, std::vector<std::string>> get_files_for_redirect(std::string cmd);
 #endif //BASHELLO_PARSE_UTILS_HPP
--- src/parse_utils.cpp
+++ src/parse_utils.cpp
@@ -2,36 +2,53 @@
 #include "../inc/builtins.hpp"
 
 
-void replace_vars(std::vector<std::string> &command) {
-//     TODO: hz czy tut ale $()
-//     треба подумати як працювати в випадку вкладень типу $($var) czy $(echo $(cat $var))
+void replace_vars(std::string&command) {
     std::vector<std::string> command_vars;
     std::string var;
+    bool write_var = false;
     char *tmp = nullptr;
-    for (auto &cmd: command) {
-        command_vars.clear();
-        var.clear();
-        bool write_var = false;
-        for (int j = 0; j < cmd.size(); ++j) {
-            if (write_var) {
-                // zabrala duzhki bo v teorii ih ne maye buty vzhe tut
-                if (!isalpha(cmd[j])) {
-                    command_vars.push_back(var);
-                    var.clear();
-                    write_var = false;
-                } else {
-                    var.push_back(cmd[j]);
-                    if (j == cmd.size() - 1) command_vars.push_back(var);
-                }
+    for (size_t j = 0; j < command.size(); ++j) {
+        if (write_var) {
+            if (!isalpha(command[j]) && !isdigit(command[j])) {
+                command_vars.push_back(var);
+                var.clear();
+                write_var = false;
             } else {
-                if (cmd[j] == '$') write_var = true;
+                var.push_back(command[j]);
+                if (j == command.size() - 1) command_vars.push_back(var);
             }
-        }
-        for (auto &v: command_vars) {
-            tmp = getenv(v.c_str());
-            boost::replace_first(cmd, "$" + v, tmp != nullptr ? tmp : "");
+        } else {
+            if (command[j] == '$') write_var = true;
         }
     }
+    for (auto &v: command_vars) {
+        tmp = getenv(v.c_str());
+        boost::replace_first(command, "$" + v, tmp != nullptr ? tmp : "");
+    }
+}
+
+std::vector<std::string> get_script_vars(std::string& command) {
+    std::vector<std::string> scripts;
+    std::string script;
+    bool write_script = false;
+    for (size_t j = 0; j < command.size(); ++j) {
+        if (write_script) {
+            if (command[j] == ')') {
+                scripts.push_back(script);
+                script.clear();
+                write_script = false;
+            } else {
+                if(command[j] != '('){
+                    script.push_back(command[j]);
+                } else{
+                    continue;
+                }
+            }
+        } else {
+            if (command[j] == '$' && command[j + 1] == '(') write_script = true;
+        }
+    }
+    return scripts;
 }
 
 void split(std::string &source, std::vector<std::string> &destination) {
@@ -94,4 +111,45 @@ int parse_variables(std::vector<std::string>& command, custom_environ &environ_)
     }
     return 0;
 
+}
+
+std::string remove_leading_ending_spaces(const std::string& str){
+    const auto start = str.find_first_not_of(' ');
+    const auto end = str.find_last_not_of(' ');
+    return str.substr(start, end-start+1);
+}
+
+std::pair<std::string, std::vector<std::string>> get_files_for_redirect(std::string cmd) {
+    std::vector<std::string>redirect_files = {"", "",""};
+    int desc;
+    std::vector<int> redirect_signs;
+    for (size_t i = 0; i < cmd.size(); ++i) {
+        if (cmd[i]=='>' || cmd[i]=='<' ) redirect_signs.emplace_back(i);
+    }
+    size_t command_end = redirect_signs[0];
+
+    for (size_t j = 0; j < redirect_signs.size(); ++j) {
+        std::string file_name;
+        size_t ind = redirect_signs[j];
+        if (cmd[ind - 1] == '0' || cmd[ind - 1] == '1' || cmd[ind - 1] == '2') {
+            desc = cmd[ind - 1] - '0';
+            if (j==0) --command_end;
+        } else {
+            desc = cmd[ind] == '<' ? 0 : 1;
+        }
+
+        if (cmd[ind+1] == '&') ++ind;
+
+        if (j == redirect_signs.size()-1){
+            file_name = cmd.substr(ind + 1);
+        }else{
+            size_t end = redirect_signs[j+1];
+            if(cmd[redirect_signs[j+1]-1] == '0' || cmd[redirect_signs[j+1]-1] == '1' || cmd[redirect_signs[j+1]-1] == '2') --end;
+            file_name = cmd.substr(ind + 1, end - ind - 1);
+        }
+        redirect_files[desc] = remove_leading_ending_spaces(file_name);
+    }
+    std::string command = remove_leading_ending_spaces(cmd.substr(0, command_end));
+    std::pair<std::string, std::vector<std::string>> new_command = std::make_pair(command, redirect_files);
+    return new_command;
 }
